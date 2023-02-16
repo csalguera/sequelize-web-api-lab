392bfb23abc0a5a266dd5a97e741ddd795188ae4
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-02-15 21:04:13 -0500

    generate album model and migrate it while referencing artist model

---
+++ models/album.js
@@ -0,0 +1,33 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Album extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Album.init({
+    name: DataTypes.STRING,
+    release: DataTypes.INTEGER,
+    artistId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      onDelete: 'CASCADE',
+      references: {
+        model: 'Artists',
+        key: 'id'
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Album',
+  });
+  return Album;
+};
