4f385d886b37548a0decf9ac667b0c741b28d737
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-02-15 21:24:17 -0500

    define route and controller action to create artist data entities

--- controllers/artists.js
+++ controllers/artists.js
@@ -1,5 +1,14 @@
 const { Artist } = require('../models')
 
+const create = async (req, res) => {
+  try {
+    const artist = await Artist.create(req.body)
+    res.status(200).json(artist)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
 module.exports = {
-  
+  create,
 }
--- routes/artists.js
+++ routes/artists.js
@@ -1,4 +1,6 @@
 const router = require('express').Router()
 const artistsCtrl = require('../controllers/artists.js')
 
+router.post('/', artistsCtrl.create)
+
 module.exports = router
