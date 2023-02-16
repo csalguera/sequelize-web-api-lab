47199f7246dc925fa57bc7bba2923eac40787f11
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-02-15 21:06:56 -0500

    define association between artist and album models

--- models/artist.js
+++ models/artist.js
@@ -10,7 +10,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Artist.hasMany(models.Album, {
+        foreignKey: 'artistId',
+        as: 'albums'
+      })
     }
   }
   Artist.init({
